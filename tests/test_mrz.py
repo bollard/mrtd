e95fd67d92c83924683dc5603af4aedde5a50bf7
bollard <358225+bollard@users.noreply.github.com>
2025-10-18 17:56:19 +0200

    bump python -> 3.13

--- tests/test_mrz.py
+++ tests/test_mrz.py
@@ -2,12 +2,16 @@ import pytest
 
 from mrtd.mrz import check_digit
 
-@pytest.mark.parametrize("chars, expected", [
-    ("AB2134<<<", 5),
-    ("559556128", 6),
-    ("900703", 5),
-    ("290327", 5),
-    ("559556128690070352903275", 0),
-])
+
+@pytest.mark.parametrize(
+    "chars, expected",
+    [
+        ("AB2134<<<", 5),
+        ("559556128", 6),
+        ("900703", 5),
+        ("290327", 5),
+        ("559556128690070352903275", 0),
+    ],
+)
 def test_check_digit(chars: str, expected: int):
     assert check_digit(chars) == expected
